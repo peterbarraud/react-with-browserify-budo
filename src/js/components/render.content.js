ad88f0c50796f6eaabf4693c92b90c9561963c4f
Peter G.A. Barraud <barraud@adobe.com>
2019-11-19 09:03:18 +0530

    comment on Fragments

--- src/js/components/render.content.js
+++ src/js/components/render.content.js
@@ -5,7 +5,8 @@ import React, { Component } from 'react';
 // used if you've got other stuff live state and events to handle
 class ClassSyntax extends Component {
     render = () => {
-        // use the empty tags to avoid injecting unnecessary divs into your content
+        // use the Fragments (shortcut) to avoid injecting unnecessary divs into your content
+        // see for more on Fragments and when to use the full syntax in place of the shortcut
         return (
             <>
                 <h1>Using the Class syntax to render</h1>
